df9457f19c23898e53a9f6ff9d3386a26de18e20
Seungil <schoi0115@gmail.com>
2021-07-04 05:45:18 -0400

    Assignment Completed

--- index.js
+++ index.js
@@ -1,26 +1,14 @@
-//for (let age = 30; age < 40; age++) {
-//  console.log(`I'm ${age} years old. Happy birthday to me!`);
-// } 
-
-//const gifts = ["teddy bear", "drone", "doll"];
-
-//function wrapGifts(gifts) {
-//  for (let i = 0; i < gifts.length; i++) {
-//   console.log(`Wrapped ${gifts[i]} and added a bow!`);
-    
-//}
-
-//  return gifts;
-//}
-
-//wrapGifts(gifts); 
+// for (let age = 30; age < 40; age++) {
+//     console.log(`I'm ${age} years old. Happy birthday to me!`);
+//     debugger;
+//   } 
 
 
-//writeCards(['Lisa', 'Kaitlin', 'Jan'], 'surprise')
 
 const arr1 = [];
 function writeCards(names, event) {
-    for (let i=0; i < names.length; i++) {
+    for (let i=0; 
+        i < names.length; i++) {
     arr1.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
     debugger;
     }
@@ -32,7 +20,7 @@ return arr1
 function countDown(int){
     let i = 10;
     while (i>= 0) {
-        console.log (i);
+        console.log(i)
         i--
     }
     return int
